591c9076b6a9a15292687cc32ed3fa06583c1450
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2025-04-14 13:01:57 +0200

    Add option to add django apps to standalone setup

--- django_oemof/standalone.py
+++ django_oemof/standalone.py
@@ -4,20 +4,21 @@ from django.conf import settings
 from django.core.management import execute_from_command_line
 
 
-def init_django():
+def init_django(installed_apps=None):
     if settings.configured:
         return
 
     env = environ.Env()
     env.read_env(".env")
 
+    installed_apps = installed_apps or []
+
     settings.configure(
         INSTALLED_APPS=[
-            'django_oemof',
-        ],
-        DATABASES={
-            'default': env.db("DATABASE_URL")
-        },
+            "django_oemof",
+        ]
+        + installed_apps,
+        DATABASES={"default": env.db("DATABASE_URL")},
         MEDIA_ROOT=env.path("MEDIA_ROOT", default="media"),
     )
     django.setup()
